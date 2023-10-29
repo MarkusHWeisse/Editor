90744fc21535c6eebbaabf7315a298a9e82931f3
MarkusHWeisse <147412030+MarkusHWeisse@users.noreply.github.com>
2023-10-29 12:56:24 +0100

    v 8
    Adding a Slider.

--- Sovieditor/main.cpp
+++ Sovieditor/main.cpp
@@ -95,12 +95,14 @@ class Text;
 class Slider {
 private:
 	bool inUse;
+	bool allowUpdate;
 	int posX;
 	int posY;
 	int changeY;
 	int defaultSize;
 	int sizeY;
 	int mPosBef;
+	int changeYClick;
 	sf::RectangleShape sliderRect;
 	sf::CircleShape circle;
 
@@ -116,12 +118,19 @@ public:
 		this->sizeY = defaultSize;
 		this->inUse = false;
 		this->mPosBef = 0;
+		this->changeY = 0;
+		this->allowUpdate = true;
 	}
 
-	void loadEvents(Editor &editor, Cursor &cursor, Text &text, sf::Event &event);
+	void loadEvents(Editor &editor, Cursor &cursor, Text &text, sf::Event &event, sf::RenderWindow &window, sf::Vector2i &mcords);
 	void loadDraw(Editor &editor, sf::RenderWindow &window);
 	void updateSlider(Editor &editor, Cursor &cursor, Text &text);
-	
+	void setChangeYToLine(Cursor &cursor, Text &text, sf::RenderWindow &window);
+	void sliderInUse(Editor &editor, Cursor &cursor, Text &text, sf::Event &event, sf::RenderWindow &window, sf::Vector2i &mcords);
+	void sliderClicked(Editor &editor, Cursor &cursor, Text &text, sf::Event &event, sf::RenderWindow &window, sf::Vector2i &mcords);
+	void setChangeYOutOfBounds(Editor &editor, Cursor &cursor, Text &text, sf::Event &event, sf::RenderWindow &window, sf::Vector2i &mcords);
+	void setChangeYInBounds(Editor &editor, Cursor &cursor, Text &text, sf::Event &event, sf::RenderWindow &window, sf::Vector2i &mcords);
+
 	void setCPosY(int posY) {
 		this->changeY = posY;
 	}
@@ -165,7 +174,7 @@ public:
 	Text() {
 		keywords = {"/*", "*/", "//", "void", "int", "double", "float", "bool", "return", "break", "switch", "case", "class", "struct", "if", "else", "while", "for", "#include", "#if",
 		"#define", "#ifndef", "#endif", "public:", "private:", "string", "std", "sf"};
-		keywordsSpecial = {"{", "}", "=", "!", "(", ")", ";", ":", "+", "-", "*", "/", "\"", ">", ","};
+		keywordsSpecial = {"{", "}", "=", "!", "(", ")", ";", ":", "+", "-", "*", "/", "\"", ">", ",", "<"};
 
 		setFontSize(14, 20);
 	}
@@ -436,40 +445,82 @@ public:
 	}
 };
 
+void Slider::setChangeYToLine(Cursor &cursor, Text &text, sf::RenderWindow &window) {
+	int maxLines = (int)((window.getSize().y - 3) / text.getFontSizeSpacing());
+	int zeile = (int)((float)changeY * (float)((float)(text.getLineSize() - maxLines) / (float)(window.getSize().y - posY - sizeY)));
+	cursor.setCursorLineNr(cursor.getCursorLineNr() + zeile + 1 - text.getBottomLine());
+	text.setBottomLine(zeile + 1);
+	allowUpdate = false;
+}
+
+void Slider::setChangeYOutOfBounds(Editor &editor, Cursor &cursor, Text &text, sf::Event &event, sf::RenderWindow &window, sf::Vector2i &mcords) {
+	if(mcords.x >= editor.getGreyBlockSize() && mcords.x <= window.getSize().x)	{
+		if(mcords.y < posY) {
+			changeY = 0;
+			setChangeYToLine(cursor, text, window);
+			return;
+		}else if(mcords.y > window.getSize().y) {
+			changeY = window.getSize().y - sizeY; 
+			setChangeYToLine(cursor, text, window);
+			return;
+		}
+	}
+}
+
+void Slider::setChangeYInBounds(Editor &editor, Cursor &cursor, Text &text, sf::Event &event, sf::RenderWindow &window, sf::Vector2i &mcords) {
+	if(mcords.x >= editor.getGreyBlockSize() && mcords.y >= 0 && mcords.x <= window.getSize().x && mcords.y <= window.getSize().y) {
+		int difference = (float)mPosBef - mcords.y;
+		int maxLines = (int)((window.getSize().y - 3) / text.getFontSizeSpacing());
+
+		if((changeYClick - difference >= posY) && (changeYClick - difference + posY + sizeY <= window.getSize().y)) {
+			changeY = changeYClick - difference;
+			setChangeYToLine(cursor, text, window);
+		}
+	}
+}
+
+void Slider::sliderInUse(Editor &editor, Cursor &cursor, Text &text, sf::Event &event, sf::RenderWindow &window, sf::Vector2i &mcords) {
+	setChangeYOutOfBounds(editor, cursor, text, event, window, mcords);
+
+	setChangeYInBounds(editor, cursor, text, event, window, mcords);
+}
+
+void Slider::sliderClicked(Editor &editor, Cursor &cursor, Text &text, sf::Event &event, sf::RenderWindow &window, sf::Vector2i &mcords) {
+	sf::Cursor c;
+	if(c.loadFromSystem(sf::Cursor::Arrow))
+		window.setMouseCursor(c);	
+	if (sf::Mouse::isButtonPressed(sf::Mouse::Button::Left) && !inUse) {
+		inUse = true;
+		mPosBef = mcords.y;
+		changeYClick = changeY;
+	}
+}
+
 void Slider::loadEvents(Editor &editor, Cursor &cursor, Text &text, sf::Event &event, sf::RenderWindow &window, sf::Vector2i &mcords) {
-	if(mcords.x >= window.getSize().x-7 && mcords.y >= posY+changeY && mcords.x <= window.getSize().x-2 && mcords.y <= posY+changeY+sizeY) {
-		sf::Cursor c;
-		if(c.loadFromSystem(sf::Cursor::Arrow))
-				window.setMouseCursor(c);	
-		if (sf::Mouse::isButtonPressed(sf::Mouse::Button::Left) && !inUse) {
-			inUse = true;
-			mPosBef = mcords.y;
-		}	
+	if(!inUse && mcords.x >= window.getSize().x-7 && mcords.y >= posY+changeY && mcords.x <= window.getSize().x-2 && mcords.y <= posY+changeY+sizeY) {
+		sliderClicked(editor, cursor, text, event, window, mcords);
 	}
 	
 	if(inUse) {
-		if(mcords.x >= editor.getGreyBlockSize() && mcords.y >= 0 && mcords.x <= window.getSize().x && mcords.y + (int)getUebersetzung() + 10 <= window->getSize().y && pos + mcords->y - mPosBef >= 0) {
-			int difference = (float)mPosBef - mcords->y;
-			
-			float ue = (float)(linesNr) / ((float)window->getSize().y - getUebersetzung());
-			
-			*touchY = (float)pos * ue;
-			*cursorPosY-=*touchY;
-			
-			pos += mcords->y - mPosBef;
-			mPosBef = mcords->y;
-		}	
+		sliderInUse(editor, cursor, text, event, window, mcords);	
 	}
 }
 
 void Slider::updateSlider(Editor &editor, Cursor &cursor, Text &text) {
-	if(text.getLineSize() < (editor.wGetSize().y - posY - defaultSize)) {
-		sizeY = (editor.wGetSize().y - posY - defaultSize) - text.getLineSize();
+	if(!allowUpdate) {
+		allowUpdate = true;
+		return;
+	}
+
+	int maxLines = (int)((editor.wGetSize().y - 3) / text.getFontSizeSpacing());
+
+	if(text.getLineSize() - maxLines < (editor.wGetSize().y - posY - defaultSize)) {
+		sizeY = (editor.wGetSize().y - posY - defaultSize) - text.getLineSize() - maxLines;
 	}else {
 		sizeY = defaultSize;
 	}
 
-	changeY = (int)((float)text.getBottomLine() * (float)((float)(editor.wGetSize().y - posY - sizeY) / (float)(text.getLineSize())));
+	changeY = (int)((float)text.getBottomLine() * (float)((float)(editor.wGetSize().y - posY - sizeY) / (float)(text.getLineSize() - maxLines)));
 }
 
 void Slider::loadDraw(Editor &editor, sf::RenderWindow &window) {
@@ -482,20 +533,20 @@ void Slider::loadDraw(Editor &editor, sf::RenderWindow &window) {
 	}
 
 	circle.setRadius(2.5);
-	circle.setPosition(sf::Vector2f(window.getSize().x - 7, posY+changeY+2.5));
+	circle.setPosition(sf::Vector2f(window.getSize().x - 7, posY+changeY));
 	circle.setFillColor(c);
 
 	window.draw(circle);
 
 	circle.setRadius(2.5);
-	circle.setPosition(sf::Vector2f(window.getSize().x - 7, posY+changeY+sizeY+2.5));
+	circle.setPosition(sf::Vector2f(window.getSize().x - 7, posY+changeY+sizeY-5));
 	circle.setFillColor(c);
 
 	window.draw(circle);
 
 	sliderRect.setFillColor(c);
-	sliderRect.setPosition(sf::Vector2f(window.getSize().x - 7, posY+changeY+5));
-	sliderRect.setSize(sf::Vector2f(5, sizeY));
+	sliderRect.setPosition(sf::Vector2f(window.getSize().x - 7, posY+changeY+2.5));
+	sliderRect.setSize(sf::Vector2f(5, sizeY-5));
 
 	window.draw(sliderRect);
 }
@@ -857,6 +908,12 @@ void Editor::loadAllEvents(sf::Event &event) {
 
 	loadEvents(event);
 	//cursor.loadEvents(*this, event, text);
+
+	slider.loadEvents(*this, cursor, text, event, window, mcords);
+
+	if (event.type == sf::Event::MouseButtonReleased) {
+		slider.escapeEvent();
+	}	
 
 	if(!getCTRL()) {
 		text.loadEvents(*this, cursor, event);
